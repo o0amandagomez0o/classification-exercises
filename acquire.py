c811f104b8a269897c9e865db59a21dc9629dbd2
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-02-22 17:18:15 -0600

    completed first half of classification exercises

---
+++ acquire.py
@@ -0,0 +1,29 @@
+#Make a function named get_titanic_data that returns the titanic data from the codeup data science database as a pandas data frame. Obtain your data from the Codeup Data Science Database.
+
+from env import host, user, password
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    
+    
+def get_titanic_data():
+    '''
+    This function reads in the titanic data from the Codeup db
+    and returns a pandas DataFrame with all columns.
+    '''
+    sql_query = 'SELECT * FROM passengers'
+    return pd.read_sql(sql_query, get_connection('titanic_db'))
+
+
+
+def get_iris_data():
+    '''
+    This function reads in the titanic data from the Codeup db
+    and returns a pandas DataFrame with all columns.
+    '''
+    sql_query = 'SELECT * FROM passengers'
+    return pd.read_sql(sql_query, get_connection('iris_db'))
