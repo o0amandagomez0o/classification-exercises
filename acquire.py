8936b019c6a54315a7092f03d20987bed1c293b3
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-02-23 09:54:51 -0600

    completed exercises

--- acquire.py
+++ acquire.py
@@ -1,4 +1,8 @@
 #Make a function named get_titanic_data that returns the titanic data from the codeup data science database as a pandas data frame. Obtain your data from the Codeup Data Science Database.
+import pandas as pd
+import numpy as np
+import os
+
 
 from env import host, user, password
 
@@ -25,5 +29,5 @@ def get_iris_data():
     This function reads in the titanic data from the Codeup db
     and returns a pandas DataFrame with all columns.
     '''
-    sql_query = 'SELECT * FROM passengers'
+    sql_query = 'select * from species join measurements using (species_id);'
     return pd.read_sql(sql_query, get_connection('iris_db'))
