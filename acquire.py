97971d10e171610dd2b0fa7383f30c8010d14413
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-02-23 10:59:39 -0600

    made corrections with review

--- acquire.py
+++ acquire.py
@@ -2,9 +2,10 @@
 import pandas as pd
 import numpy as np
 import os
-
-
 from env import host, user, password
+
+
+
 
 def get_connection(db, user=user, host=host, password=password):
     '''
@@ -31,3 +32,49 @@ def get_iris_data():
     '''
     sql_query = 'select * from species join measurements using (species_id);'
     return pd.read_sql(sql_query, get_connection('iris_db'))
+
+
+
+def rf_titanic_data(cached=False):
+    '''
+    This function reads in titanic data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in titanic df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('titanic.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = get_titanic_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('titanic.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('titanic.csv', index_col=0)
+        
+    return df
+
+
+
+def rf_iris_data(cached=False):
+    '''
+    This function reads in titanic data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in titanic df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('iris.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = rf_iris_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('iris.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('iris.csv', index_col=0)
+        
+    return df
