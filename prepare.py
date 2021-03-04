928cac7244e8200d293db5dddfee7c12f72e1249
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-03-04 17:20:51 -0600

    still working on different models to find ONE to test

--- prepare.py
+++ prepare.py
@@ -137,3 +137,22 @@ def prep_titanic(df):
     train_validate, test = train_test_split(df, test_size=0.2, random_state=3210, stratify=df.survived)
     train, validate = train_test_split(train_validate, train_size=0.7, random_state=3210, stratify=train_validate.survived)
     return train, validate, test
+
+
+
+def train_validate_test_split(df, target, seed=3210):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
